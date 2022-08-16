689cc82cd5fc94ceb53e36085ad02d00fc307779
Cyberium <cyberium@users.noreply.github.com>
2022-08-16 22:15:52 +0200

    Revert "Pathfinder: Store not found poly in poly cache"
    This reverts commit 464f72f74f757c265404dcabf7c71041892d0c7f.

--- src/game/MotionGenerators/PathFinder.h
+++ src/game/MotionGenerators/PathFinder.h
@@ -149,7 +149,7 @@ class PathFinder
         bool inRangeYZX(const float* v1, const float* v2, float r, float h) const;
 
         dtPolyRef getPathPolyByPosition(const dtPolyRef* polyPath, uint32 polyPathSize, const float* point, float* distance = nullptr) const;
-        dtPolyRef getPolyByLocation(const float* point, float* distance);
+        dtPolyRef getPolyByLocation(const float* point, float* distance) const;
         bool HaveTile(const Vector3& p) const;
 
         void BuildPolyPath(const Vector3& startPos, const Vector3& endPos);
