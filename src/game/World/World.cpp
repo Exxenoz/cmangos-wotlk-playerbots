36c8ec18438c9022c73a5a7930a2887298ac4901
celguar <celguar@gmail.com>
2022-11-26 15:06:31 +0300

    Do not logout bots

--- src/game/World/World.cpp
+++ src/game/World/World.cpp
@@ -2485,7 +2485,7 @@ void World::ShutdownServ(uint32 time, uint32 options, uint8 exitcode)
     }
 
 #ifdef ENABLE_PLAYERBOTS
-    sRandomPlayerbotMgr.LogoutAllBots();
+    //sRandomPlayerbotMgr.LogoutAllBots();
 #endif
 
 }
